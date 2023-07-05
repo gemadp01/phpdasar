59714c96b98e3aed5b12ccdb4b2caa675c15f15e
Gema Dodi Pranata <gemadp01@gmail.com>
2023-07-05 10:15:35 +0700

    Add Folder pertemuan 11 - Update

---
+++ pertemuan11-Update/index.php
@@ -0,0 +1,62 @@
+<?php
+require 'functions.php';
+
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+
+<body>
+
+    <h1>Daftar Mahasiswa</h1>
+
+    <a href="tambah.php">Tambah data mahasiswa</a>
+    <br><br>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+        <thead>
+
+            <tr>
+                <th>No.</th>
+                <th>Aksi</th>
+                <th>Gambar</th>
+                <th>Nama</th>
+                <th>NPM</th>
+                <th>Email</th>
+                <th>Jurusan</th>
+            </tr>
+        </thead>
+        <tbody>
+
+            <?php $i = 1; ?>
+            <?php foreach ($mahasiswa as $row) : ?>
+                <tr>
+                    <td><?= $i; ?></td>
+                    <td>
+                        <a href="ubah.php?id=<?= $row["id"]; ?>">Ubah</a> ||
+                        <a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('Do you want to delete?')">Hapus</a>
+                    </td>
+                    <td>
+                        <img src="img/<?= $row["gambar"]; ?>" alt="<?= $row["nama"]; ?>" width="50">
+                    </td>
+                    <td><?= $row["npm"]; ?></td>
+                    <td><?= $row["nama"]; ?></td>
+                    <td><?= $row["email"]; ?></td>
+                    <td><?= $row["jurusan"]; ?></td>
+                </tr>
+                <?php $i++; ?>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
+</body>
+
+</html>
