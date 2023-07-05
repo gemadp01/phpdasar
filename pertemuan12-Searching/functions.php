0397dbc3f3e21cea064c8619b26a0562c2d65034
Gema Dodi Pranata <gemadp01@gmail.com>
2023-07-05 23:17:33 +0700

    Add Folder pertemuan 12 - Searching

---
+++ pertemuan12-Searching/functions.php
@@ -0,0 +1,78 @@
+<?php
+//Koneksi ke database
+$conn = mysqli_connect("localhost", "root", "", "phpdasar");
+
+function query($query)
+{
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    // var_dump($result);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+};
+
+
+function tambah($data)
+{
+    global $conn;
+    $npm = htmlspecialchars($data["npm"]);
+    $nama = htmlspecialchars($data["nama"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+    $gambar = htmlspecialchars($data["gambar"]);
+
+    $query = "INSERT INTO mahasiswa
+            VALUES
+            ('', '$npm', '$nama', '$email', '$jurusan', '$gambar')
+            ";
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+function hapus($id)
+{
+    global $conn;
+    mysqli_query($conn, "DELETE FROM mahasiswa WHERE id = $id");
+    return mysqli_affected_rows($conn);
+}
+
+function ubah($data)
+{
+    global $conn;
+
+    $id = $data["id"];
+    $npm = htmlspecialchars($data["npm"]);
+    $nama = htmlspecialchars($data["nama"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+    $gambar = htmlspecialchars($data["gambar"]);
+
+    $query = "UPDATE mahasiswa
+            SET
+            npm = '$npm',
+            nama = '$nama',
+            email = '$email',
+            jurusan = '$jurusan',
+            gambar = '$gambar'
+            WHERE id = $id
+            ";
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+function cari($keyword)
+{
+    $query =  "SELECT * FROM mahasiswa
+                    WHERE
+                nama LIKE '%$keyword%' OR
+                npm LIKE '%$keyword%' OR
+                email LIKE '%$keyword%' OR
+                jurusan LIKE '%$keyword%'
+            ";
+    return query($query);
+}
---
+++ pertemuan12-Searching/ubah.php
@@ -0,0 +1,77 @@
+<?php
+require 'functions.php';
+
+// ambil data di url
+$id = $_GET["id"];
+
+// query data mahasiswa berdasarkan id
+$mhs = query("SELECT * FROM mahasiswa WHERE id = $id")[0];
+
+
+// cek apakah tombol submit sudah ditekan atau belum
+if (isset($_POST["submit"])) {
+
+    // cek apakah data berhasil di ubah atau tidak
+    if (ubah($_POST) > 0) {
+        echo "
+            <script>
+                alert('data berhasil diubah!');
+                document.location.href = 'index.php';
+            </script>
+        ";
+    } else {
+        echo "
+            <script>
+                alert('data gagal diubah!');
+                document.location.href = 'index.php';
+            </script>
+        ";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ubah data mahasiswa</title>
+</head>
+
+<body>
+
+    <h1>Ubah data mahasiswa</h1>
+
+    <form action="" method="post">
+        <ul>
+            <input type="hidden" name="id" value="<?= $mhs["id"]; ?>">
+            <li>
+                <label for="npm">NPM : </label>
+                <input type="text" name="npm" id="npm" placeholder="NPM" required value="<?= $mhs["npm"]; ?>">
+            </li>
+            <li>
+                <label for="nama">Nama : </label>
+                <input type="text" name="nama" id="nama" placeholder="Nama" required value="<?= $mhs["nama"]; ?>">
+            </li>
+            <li>
+                <label for="email">Email : </label>
+                <input type="text" name="email" id="email" placeholder="Email" value="<?= $mhs["email"]; ?>">
+            </li>
+            <li>
+                <label for="jurusan">Jurusan : </label>
+                <input type="text" name="jurusan" id="jurusan" placeholder="Jurusan" required value="<?= $mhs["jurusan"]; ?>">
+            </li>
+            <li>
+                <label for="gambar">Gambar : </label>
+                <input type="text" name="gambar" id="gambar" value="<?= $mhs["gambar"]; ?>">
+            </li>
+            <li>
+                <button type="submit" name="submit">Ubah Data!</button>
+            </li>
+        </ul>
+    </form>
+
+
+</body>
+
+</html>
