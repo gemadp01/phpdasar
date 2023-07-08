56d2509b510ed8aff90dd44e365fdef2954d7aa3
Gema Dodi Pranata <gemadp01@gmail.com>
2023-07-08 15:56:46 +0700

    Memodifikasi pertemuan12 - searching/tambah.php

--- pertemuan12-Searching/tambah.php
+++ pertemuan12-Searching/tambah.php
@@ -1,69 +1,69 @@
-<?php
-require 'functions.php';
+ <?php
+    require 'functions.php';
 
-// cek apakah tombol submit sudah ditekan atau belum
-if (isset($_POST["submit"])) {
+    // cek apakah tombol submit sudah ditekan atau belum
+    if (isset($_POST["submit"])) {
 
-    // cek apakah data berhasil di tambahkan atau tidak
-    if (tambah($_POST) > 0) {
-        echo "
+        // cek apakah data berhasil di tambahkan atau tidak
+        if (tambah($_POST) > 0) {
+            echo "
             <script>
                 alert('data berhasil ditambahkan!');
                 document.location.href = 'index.php';
             </script>
         ";
-    } else {
-        echo "
+        } else {
+            echo "
             <script>
                 alert('data gagal ditambahkan!');
                 document.location.href = 'index.php';
             </script>
         ";
+        }
     }
-}
-?>
-<!DOCTYPE html>
-<html lang="en">
+    ?>
+ <!DOCTYPE html>
+ <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah data mahasiswa</title>
-</head>
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Tambah data mahasiswa</title>
+ </head>
 
-<body>
+ <body>
 
-    <h1>Tambah data mahasiswa</h1>
+     <h1>Tambah data mahasiswa</h1>
 
-    <form action="" method="post">
-        <ul>
-            <li>
-                <label for="npm">NPM : </label>
-                <input type="text" name="npm" id="npm" placeholder="NPM" required>
-            </li>
-            <li>
-                <label for="nama">Nama : </label>
-                <input type="text" name="nama" id="nama" placeholder="Nama" required>
-            </li>
-            <li>
-                <label for="email">Email : </label>
-                <input type="text" name="email" id="email" placeholder="Email">
-            </li>
-            <li>
-                <label for="jurusan">Jurusan : </label>
-                <input type="text" name="jurusan" id="jurusan" placeholder="Jurusan" required>
-            </li>
-            <li>
-                <label for="gambar">Gambar : </label>
-                <input type="text" name="gambar" id="gambar">
-            </li>
-            <li>
-                <button type="submit" name="submit">Tambah Data!</button>
-            </li>
-        </ul>
-    </form>
+     <form action="" method="post">
+         <ul>
+             <li>
+                 <label for="npm">NPM : </label>
+                 <input type="text" name="npm" id="npm" placeholder="NPM" required>
+             </li>
+             <li>
+                 <label for="nama">Nama : </label>
+                 <input type="text" name="nama" id="nama" placeholder="Nama" required>
+             </li>
+             <li>
+                 <label for="email">Email : </label>
+                 <input type="text" name="email" id="email" placeholder="Email">
+             </li>
+             <li>
+                 <label for="jurusan">Jurusan : </label>
+                 <input type="text" name="jurusan" id="jurusan" placeholder="Jurusan" required>
+             </li>
+             <li>
+                 <label for="gambar">Gambar : </label>
+                 <input type="text" name="gambar" id="gambar">
+             </li>
+             <li>
+                 <button type="submit" name="submit">Tambah Data!</button>
+             </li>
+         </ul>
+     </form>
 
 
-</body>
+ </body>
 
-</html>
+ </html>
